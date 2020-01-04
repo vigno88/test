e28f6d439ded323726fee13cd7ce8242cc051f01
nathan <nathan.alix88@gmail.com>
2020-01-04 09:38:27 -0500

    .

--- serial.go
+++ serial.go
@@ -17,49 +17,60 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	n, err := port.Write([]byte("#CNCGCODE000\n"))
+	n, err := port.Write([]byte("#___CNCON000\n"))
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Sent %v bytes\n", n)
-	time.Sleep(200* time.Millisecond)
-	gc := make([]string,5)
-	gc[0] = "G0X20Y20"
-	gc[1] = "G0X-20Y20"
-	gc[2] = "G0X-20Y-20"
-	gc[3] = "G0X20Y-20"
-	gc[4] = "G0X0Y0"
-
-	printLine := true
-	i := 0
-
-
 	buff := make([]byte, 100)
-	for {
-		if printLine {
-			port.Write([]byte(gc[i]))
-			fmt.Println(gc[i])
-			printLine = false
-			i++
-		}
-		n, err := port.Read(buff)
+
+	n, err = port.Read(buff)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp := fmt.Sprintf("%v", string(buff[:n]))
+	fmt.Println(resp)
+	if resp == "ok" {
+		n, err := port.Write([]byte("#CNCGCODE000\n"))
 		if err != nil {
 			log.Fatal(err)
-			break
 		}
-		if n == 0 {
-			fmt.Println("\nEOF")
-			break
-		}
-		resp := fmt.Sprintf("%v", string(buff[:n]))
-		fmt.Println(resp)
-		if resp == "ok" {
-			printLine = true
-		}
-		if i > 5 {
-			break
-		}
+		fmt.Printf("Sent %v bytes\n", n)
+		time.Sleep(200* time.Millisecond)
+		gc := make([]string,5)
+		gc[0] = "G0X20Y20"
+		gc[1] = "G0X-20Y20"
+		gc[2] = "G0X-20Y-20"
+		gc[3] = "G0X20Y-20"
+		gc[4] = "G0X0Y0"
 
+		printLine := true
+		i := 0
+
+		for {
+			if printLine {
+				port.Write([]byte(gc[i]))
+				fmt.Println(gc[i])
+				printLine = false
+				i++
+			}
+			n, err := port.Read(buff)
+			if err != nil {
+				log.Fatal(err)
+				break
+			}
+			if n == 0 {
+				fmt.Println("\nEOF")
+				break
+			}
+			resp := fmt.Sprintf("%v", string(buff[:n]))
+			fmt.Println(resp)
+			if resp == "ok" {
+				printLine = true
+			}
+			if i > 5 {
+				break
+			}
+		}
 	}
 	fmt.Println("Salut")
 
